ed3a061444454a7fb8763fbaf831758c5440ddfa
Jefferson Matheus <matheus.jefferson@escolar.ifrn.edu.br>
2026-02-08 21:05:13 -0300

    feat: sparkles: Criação da função para criar diretorios relacionados as downloads

--- systemBuild.sh
+++ systemBuild.sh
@@ -1,2 +1,19 @@
 #!/bin/env bash
 
+echo "###########################"
+echo "MONTANDO O SISTEMA"
+echo "###########################"
+
+ROOT_PAHT="/home/$(whoami)"
+
+build_download_structure(){
+    echo "Mudando para o diretorio de downloads"
+    cd $ROOT_PAHT/Downloads
+
+    DOWNLOAD_FOLDERS_STRUCTURE=("temas_zip_tar" "arquivos_deb" "arquivos_iso")
+
+    for directory in "${DOWNLOAD_FOLDERS_STRUCTURE[@]}"; do
+        mkdir $directory
+    done
+}
+build_download_structure
